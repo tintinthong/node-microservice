16718a43b8a2653c70e9c1c330a46f0a7ca98a5e
Justin <justinthong93@gmail.com>
2019-09-05 01:58:42 +0800

    adding stuff

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker rm -f nazmul_node_micro
-# docker rmi nazmulb/node-microservice
-# docker pull nazmulb/node-microservice
-# docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+docker rmi nazmulb/node-microservice
+docker pull nazmulb/node-microservice
+docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
 
