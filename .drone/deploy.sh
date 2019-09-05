1d99f4188cbf69982561a0377b59c33204b5c23a
Justin <justinthong93@gmail.com>
2019-09-05 22:39:13 +0800

    just leaving with docker pull

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-docker rm -f nazmul_node_micro
-docker rmi nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 # docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
 
