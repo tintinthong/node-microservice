65936e89772baf236bb56602e7a0032d31973d33
Justin <justinthong93@gmail.com>
2019-09-06 01:49:36 +0800

    trying base64 again

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -4,7 +4,7 @@ if [ "$(docker ps -aq -f status=exited -f name=nazmul_node_micro)" ]; then
         # cleanup
         docker rm -f nazmul_node_micro
 fi
-docker rmi nazmulb/node-microservice
+docker rmi -f nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 docker run --rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
 
