5b3374bea0fed9745c5f986c09e5fd733d24c53d
Justin <justinthong93@gmail.com>
2019-09-05 23:34:24 +0800

    pull and run

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker run hello-world
-# docker pull nazmulb/node-microservice
-# docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
+#docker run hello-world
+docker pull nazmulb/node-microservice
+docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
 
