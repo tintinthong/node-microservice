7b9765fda5e8900b893ee3bfb2f8bd2f3bf84734
Justin <justinthong93@gmail.com>
2019-09-06 01:52:43 +0800

    trying again adlknfasf

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-if [ "$(docker ps -aq -f status=exited -f name=nazmul_node_micro)" ]; then
-        # cleanup
-        docker rm -f nazmul_node_micro
-fi
+docker rm -f nazmul_node_micro
 docker rmi -f nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 docker run --rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
