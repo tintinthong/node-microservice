a84957ad02f113fa49010fc86a569c9afa5cf917
Justin <justinthong93@gmail.com>
2019-09-05 23:47:16 +0800

    deploy

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-#docker run hello-world
-docker rm -f nazmul_node_micro
+if [ "$(docker ps -aq -f status=exited -f name=nazmul_node_micro)" ]; then
+        # cleanup
+        docker rm -f nazmul_node_micro
+fi
 docker rmi nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 docker run --rm -p -d 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
