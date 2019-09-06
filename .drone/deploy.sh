7dc38c970485f462fb287fc830a5673ec41c4171
Justin <justinthong93@gmail.com>
2019-09-06 13:00:56 +0800

    try a push

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-docker rm -f nazmul_node_micro
+if [ "$(docker ps -aq -f name=<nazmul_node_micro>)" ]; then
+        # cleanup
+        docker rm <nazmul_node_micro>
+fi
+ 
+#docker rm -f nazmul_node_micro
 docker rmi -f nazmulb/node-microservice
 docker pull nazmulb/node-microservice
 docker run --rm -d -p 7777:3000 --name nazmul_node_micro nazmulb/node-microservice
